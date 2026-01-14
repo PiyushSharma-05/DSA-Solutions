c02f3c62f9eba77e80534638ff1f1bfb7e0c256b
Piyush Sharma <piyusht0510@gmail.com>
2026-01-15 01:37:03 +0530

    bubble selection insertion sorting algo

---
+++ n^2_sorting.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+//n-1 iterations
+void bubbleSort(int arr[], int n){
+    for(int i=0; i<n; i++){
+        bool isSwap=false;
+        for(int j=0; j<n; j++){
+            if(arr[j]>arr[j+1]){
+                swap(arr[j],arr[j+1]);
+                isSwap=true;
+            }
+        }
+        if(!isSwap) return ;
+    }
+}
+void selectionSort(int arr[], int n){
+    for(int i=0; i<n-1; i++){
+        int smallestIdx=i;
+        for(int j=i+1; j<n; j++){
+            if(arr[j]<arr[smallestIdx]){
+                smallestIdx=j;
+            }
+        }
+        swap(arr[smallestIdx],arr[i]);
+    }
+}
+void insertionSort(int arr[], int n){
+    for(int i=1; i<n; i++){
+        int curr=arr[i];
+        int prev=i-1;
+
+        while(prev>=0 && arr[prev]>curr){
+            arr[prev+1]=arr[prev];
+            prev--;
+        }
+        arr[prev+1] = curr;
+    }
+}
+void printArray(int arr[], int n){
+    for(int i=0; i<n; i++){
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main(){
+int n=5;
+int arr[5]={4,1,5,2,3};
+
+bubbleSort(arr,n);
+selectionSort(arr,n);
+insertionSort(arr,n);
+printArray(arr,n);
+
+    return 0;
+}
