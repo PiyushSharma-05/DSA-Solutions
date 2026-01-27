c6017ff0319509900592b056692b499ef1963495
Piyush Sharma <piyusht0510@gmail.com>
2026-01-28 00:00:13 +0530

    merge sort algorithm

---
+++ 0000_merge_sort.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void merge(vector<int>& arr, int st, int mid, int end){
+     vector<int> temp;
+     int i = st, j = mid+1;
+
+     while(i <= mid && j <= end){
+        if(arr[i] <= arr[j]){
+            temp.push_back(arr[i]);
+            i++;
+        }else{
+            temp.push_back(arr[j]);
+            j++;
+        }
+     }
+     while( i <= mid ){
+        temp.push_back(arr[i]);
+        i++;
+     }
+     while( j <= end ){
+        temp.push_back(arr[j]);
+        j++;
+     }
+
+     for(int idx=0; idx<temp.size(); idx++){
+        arr[idx+st] = temp[idx];
+     }
+
+
+}
+
+void mergeSort(vector<int>& arr, int st, int end){
+    if(st < end){
+    int mid = st + (end-st)/2;
+
+    mergeSort(arr, st, mid);
+    mergeSort(arr, mid+1, end);
+
+    merge(arr, st, mid, end);
+    }
+}
+
+int main(){
+
+    vector<int> arr = {8,1,7,3,4,10,9,5,2};
+    int n = arr.size();
+    mergeSort(arr, 0, n-1);
+
+    for(int val : arr){
+        cout << val << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
