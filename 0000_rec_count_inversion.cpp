1501bb605cbfdc17dd2598b07d3823efa79a6a8a
Piyush Sharma <piyusht0510@gmail.com>
2026-01-28 22:29:18 +0530

    recursive count inversion

---
+++ 0000_rec_count_inversion.cpp
@@ -0,0 +1,66 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int merge(vector<int>& arr, int st, int mid, int end){
+    vector<int> temp;
+    int i = st, j = mid+1;
+    int invCount = 0;
+
+    while( i<= mid && j<=end ){
+        if(arr[i] <= arr[j]){
+            temp.push_back(arr[i]);
+            i++;
+        } else{
+            temp.push_back(arr[j]);
+            j++;
+            invCount += (mid-i+1);
+        }
+    }
+    while( i <= mid ){
+        temp.push_back(arr[i]);
+        i++;
+    }
+    while( j <= end ){
+        temp.push_back(arr[j]);
+        j++;
+    }
+
+    for(int idx=0; idx<temp.size(); idx++){
+        arr[idx+st] = temp[idx];
+    }
+
+    return invCount;
+}
+
+int mergeSort(vector<int>& arr, int st, int end){
+    if( st < end){
+        int mid = st + (end-st)/2;
+
+       int leftCount = mergeSort(arr, st, mid);
+       int rightCount = mergeSort(arr, mid+1, end);
+
+       int invCount = merge(arr, st, mid, end);
+
+       return invCount + leftCount + rightCount;
+    }
+
+    return 0;
+}
+
+int main(){
+vector<int> arr = {6,3,5,2,7};
+vector<int> arr2 = {1,3,5,10,2,4,6,8,9};
+
+cout << mergeSort(arr, 0, arr.size()) << endl ;
+cout << mergeSort(arr2, 0, arr2.size()) << endl ;
+
+// mergeSort(arr, 0, arr.size());
+
+// for(int val : arr){
+//     cout << val << " ";
+// }
+// cout << endl ;
+
+    return 0;
+}
