245de41421e2e67d7f653ac9cdb710b1399b7a5b
Piyush Sharma <piyusht0510@gmail.com>
2026-02-02 18:41:36 +0530

    recursive swap nodes of linkedlist in piar(done by me)

---
+++ 0024_rec_swapNodes_in_pair_linkedlist.cpp
@@ -0,0 +1,38 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        ListNode* curr = head;
+        int count = 0;
+        while(count < 2){
+            if(curr == NULL){
+                return head;
+            } else if(curr->next == NULL && count == 0){
+                return curr;
+            }
+            curr = curr->next;
+            count++;
+        }
+        ListNode* prevNode = swapPairs(curr);
+
+        curr = head, count = 0;
+        while(count < 2){
+            ListNode* next = curr->next;
+            curr->next = prevNode;
+
+            prevNode = curr;
+            curr = next;
+            count++;
+        }
+        return prevNode;
+    }
+};
