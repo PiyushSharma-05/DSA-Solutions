7911000103eba8f5b605f67b9a99af9bf0c1adf3
Piyush Sharma <piyusht0510@gmail.com>
2026-08-09 19:28:19 +0530

    with a bit more optimised code

--- 0142_index_of_cycleStart_linkedlist.cpp
+++ 0142_index_of_cycleStart_linkedlist.cpp
@@ -1,4 +1,3 @@
-
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -10,29 +9,23 @@
 class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {
-        ListNode* slow = head;
-        ListNode* fast = head;
-        bool isCycle = false;
-        while(fast != NULL && fast->next != NULL){
+        ListNode *slow = head;
+        ListNode *fast = head;
+
+        while(fast!=nullptr && fast->next!=nullptr){
             slow = slow->next;
             fast = fast->next->next;
-            if(slow == fast){
-                isCycle = true;
-                break;
-            }
-        }
-            if(!isCycle){
-                return NULL;
-            }
-            slow = head;
-            ListNode* prev = NULL;
-            while(slow != fast){
-                slow = slow->next;
-                prev = fast;
-                fast = fast->next;
-            }
 
-            prev->next= NULL;
-            return slow;
+            if(slow == fast){
+                slow = head;
+                while(slow != fast){
+                    slow = slow->next;
+                    fast = fast->next;
+                }
+                return slow;
+            }
         }
+        return nullptr;
+    }
+
 };
