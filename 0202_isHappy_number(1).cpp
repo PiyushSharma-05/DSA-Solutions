b0c94ae46dd91bff3a22a1ce577ecd3cf4eadac7
Piyush Sharma <piyusht0510@gmail.com>
2026-08-09 19:18:09 +0530

    Using slow and fast pointers o(1) space

---
+++ 0202_isHappy_number(1).cpp
@@ -0,0 +1,24 @@
+class Solution {
+private: 
+    int getNext(int n){
+        int totalSum = 0;
+        while(n>0){
+            int d = n % 10;
+            totalSum += d*d;
+            n /= 10;
+        }
+        return totalSum;
+    }
+
+public:
+    bool isHappy(int n) {
+        int slow = n;
+        int fast = getNext(n);
+
+        while(slow!=1 && slow!=fast){
+            slow = getNext(slow);
+            fast = getNext(getNext(fast));
+        }
+        return slow == 1;
+    }
+};
